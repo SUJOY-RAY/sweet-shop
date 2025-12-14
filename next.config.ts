81fdba5665f5ab81f420fcc32ae3c3eb1ccb37c6
SUJOY RAY <sujoygithub@gmail.com>
2025-12-14 14:47:18 +0530

    fixed the issue of sweet image url, set it to every possible url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains:['media.istockphoto.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   }
 };
 
